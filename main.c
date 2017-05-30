1ea169a3ed69c196ef9521b6ae95093e6c22238a
José Vitor Hisse <jhisse@users.noreply.github.com>
2017-05-30 14:08:42 -0300

    Verifica se o chute foi maior ou menor que o número secreto
    Verifica se o chute foi menor ou maior que o número secreto caso o usuário tenha errado o número e passa uma mensagem.

--- main.c
+++ main.c
@@ -13,13 +13,22 @@ int main() {
 
     printf("Qual o seu chute? ");
     scanf("%d", &chute);
-    printf("Seu chute foi %d.", chute);
+    printf("Seu chute foi %d.\n", chute);
 
+    int acertou = (chute == numero_secreto);
 
-    if (chute == numero_secreto) {
+    if (acertou) {
         printf("Parabéns! Você acertou!");
     } else {
-        printf("Você errou!");
+
+        int maior = (chute > numero_secreto);
+
+        if (maior) {
+            printf("Você errou! O número que você chutou é maior que o número secreto.");
+        } else {
+            printf("Você errou! O número que você chutou é menor que o número secreto.");
+        };
+
     };
 
 }
