ec7315cfd8939b772eed8bdafcb6f3868af015b3
José Vitor Hisse <jhisse@users.noreply.github.com>
2017-05-30 18:26:10 -0300

    Implementação do nível de dificuldade
    Implementação do nível de dificuldade usando o switch.

--- main.c
+++ main.c
@@ -20,8 +20,30 @@ int main() {
 
     double pontos = 1000;
 
-    while (1) {
-        printf("Tentativa %d\n", tentativa);
+    int acertou = 0;
+
+    int nivel;
+    printf("Qual o nível de dificuldade?\n");
+    printf("(1) Fácil (2) Médio (3) Difícil\n\n");
+    printf("Escolha: ");
+    scanf("%d", &nivel);
+
+    int maximo_de_tentativas;
+
+    switch (nivel) {
+        case 1:
+            maximo_de_tentativas = 20;
+            break;
+        case 2:
+            maximo_de_tentativas = 15;
+            break;
+        default:
+            maximo_de_tentativas = 6;
+            break;
+    };
+
+    for (tentativa; tentativa <= maximo_de_tentativas; tentativa++) {
+        printf("Tentativa %d de %d\n", tentativa, maximo_de_tentativas);
         printf("Qual o seu chute? ");
 
         scanf("%d", &chute);
@@ -31,6 +53,8 @@ int main() {
         if (chute < 0) {
             printf("Você não pode chutar números negativos.\n");
 
+            tentativa--;
+
             continue; // pula para a próxima iteração do while
         };
 
@@ -39,23 +63,25 @@ int main() {
         int maior = (chute > numero_secreto);
 
         if (acertou) {
-            printf("Parabéns! Você acertou!\n");
-
-            break; // para o laço while
+            break; // para o laço
         } else if (maior) {
             printf("Você errou! O número que você chutou é maior que o número secreto.\n");
         } else {
             printf("Você errou! O número que você chutou é menor que o número secreto.\n");
         };
 
-        tentativa++;
-
         double pontosperdidos = abs(chute - numero_secreto) / (double)2;
         pontos -= pontosperdidos;
 
-    }; // end while
+    }; // end for
 
     printf("Fim de jogo!\n");
-    printf("Você acertou em %d tentativas.\n", tentativa);
-    printf("Total de pontos : %.1f\n", pontos);
+
+    if (acertou) {
+        printf("Você ganhou! Parabéns!");
+        printf("Você acertou em %d tentativas.\n", tentativa);
+        printf("Total de pontos : %.1f\n", pontos);
+    } else {
+        printf("Você perdeu. Tente denovo.");
+    };
 }
